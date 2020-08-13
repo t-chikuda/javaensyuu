e0fff0ca91f5e61673ee66727b3c4e5394d69535
t-chikuda <t-chikuda@esm.co.jp>
2020-08-14 00:30:59 +0900

    Practice2

---
+++ Practice2_1.java
@@ -0,0 +1,7 @@
+public class Practice2_1 {
+    public static void main(String[] args) {
+        double x;
+        x = 5 * 3.14;
+        System.out.println("計算結果は" + x);
+    }
+}
---
+++ Practice2_4.java
@@ -0,0 +1,8 @@
+public class Practice2_4 {
+    public static void main(String[] args) {
+        double a = 7;
+        double b = 2;
+        double d = a / b;
+        System.out.println(d);
+    }
+}
