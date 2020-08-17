12cb705b356f2f0a0f07134ce92a70b8411b1b9c
t-chikuda <t-chikuda@esm.co.jp>
2020-08-17 21:31:47 +0900

    Practice3

---
+++ Practice3_2.java
@@ -0,0 +1,10 @@
+public class Practice3_2 {
+    public static void main(String[] args) {
+        int a = 2011;
+        if (a % 3 == 0){
+            System.out.println("3で割り切れます");
+        }else{
+            System.out.println("3で割り切れません");
+        }
+    }
+}
