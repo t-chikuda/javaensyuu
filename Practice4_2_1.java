b1e929beb268e545a826fd7c2ea87153cc1fb9b9
t-chikuda <t-chikuda@esm.co.jp>
2020-08-25 00:05:29 +0900

    Practice4

--- Practice4_2_1.java
+++ Practice4_2_1.java
@@ -5,7 +5,7 @@ class Student{
 
 public class Practice4_2_1 {
     public static void main(String[] args) {
-        Student s =new Student();
+        Student s = new Student();
         s.name = "鈴木太郎";
         s.age = 17;
     }
