14f309008d6d46cfa0449165aeedb5ca147afd0f
t-chikuda <t-chikuda@esm.co.jp>
2020-08-25 00:08:01 +0900

    Practice3

--- Practice3_3.java
+++ Practice3_3.java
@@ -3,19 +3,16 @@ public class Practice3_3 {
         int b = 0;
 
         for (int a = 10; a <= 20; a++) {
-            b = b + a;
+            b += a;
         }
         System.out.println(b);
 
         int c = 10;
         int d = 0;
-            while (c <= 20){
+        while (c <= 20) {
             d = d + c;
             c++;
         }
         System.out.println(d);
-}
-
-
-
+    }
 }
