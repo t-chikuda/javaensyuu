177b3e285117d349ef4e003ab9d1fc7a7256c633
t-chikuda <t-chikuda@esm.co.jp>
2020-08-25 00:23:35 +0900

    Practice6

---
+++ Practice6_4.java
@@ -0,0 +1,8 @@
+public class sumpleCale {
+    static double getTriangleAres(double base, double height){
+        return base * height / 2.0;
+    }
+    static double getSquaredValue(double a){
+        return a * a;
+    }
+}
