fd677f13e2bcc8dfaa4db079ebeb7aa338ed318a
t-chikuda <t-chikuda@esm.co.jp>
2020-09-09 21:23:48 +0900

    Practice18

---
+++ Practice18_2.java
@@ -0,0 +1,8 @@
+import static java.lang.Math.*;
+
+public class Example {
+    public static void main(String[] args) {
+        System.out.println("E=" + E );
+        System.out.println("PI=" + PI );
+    }
+}
